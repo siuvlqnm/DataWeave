22bf069490db941d9f4b39501fe0117a6d3699f5
siuvlqnm <siuvlqnm@users.noreply.github.com>
2025-01-10 15:05:45 +0800

    firest commit

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+// 主视图
+//  ContentView.swift
+//  DataWeave
+//
+//  Created by Meat on 2025/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataWeaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataWeaveApp.swift
+//  DataWeave
+//
+//  Created by Meat on 2025/1/8.
+//
+
+import SwiftUI
+
+@main
+struct DataWeaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
